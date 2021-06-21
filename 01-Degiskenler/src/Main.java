5d1460c1dcacaf98287b66c8050f6f8c24c48506
HimmelKonig <80010235+HimmelKonig@users.noreply.github.com>
2021-06-22 00:50:12 +0300

    Add files via upload

---
+++ 01-Degiskenler/src/Main.java
@@ -0,0 +1,39 @@
+// Degiskenler
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println(a);
+        
+        // Not Not Not
+        /*
+        Not
+        Not
+        Not
+        */
+        
+        int b;
+        b=6;
+        System.out.println(b);
+        
+        int c= 5+4;
+        System.out.println(c);
+        
+        int d = 2;
+        int e = 3;
+        int f = 4;
+        
+        int toplam = d + 2*e +f;
+        System.out.println(toplam);
+        System.out.println("Toplam =" +  " " + toplam);
+        
+        int g = 10;
+        int h = 2 * g;
+        int k = h - 13;
+        int toplam1 = g + h + k;
+                
+        System.out.println("Toplam Deger = "+ toplam1);
+        
+        
+    }
+}
