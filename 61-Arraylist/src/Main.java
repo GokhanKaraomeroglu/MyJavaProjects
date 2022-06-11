5481e78d885fa8f26b7889471f727feb69e4c9fd
Gökhan Karaömeroğlu <80010235+GokhanKaraomeroglu@users.noreply.github.com>
2022-06-11 12:16:45 +0300

    Add files via upload

---
+++ 61-Arraylist/src/Main.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void yazdir(ArrayList<String> a) {
+        
+        for (int i = 0; i < a.size() ; i++) {
+            
+            System.out.println("Element " + (i+1) + ": " + a.get(i));
+            
+        }
+    }
+    public static void yaz (ArrayList<String> array){ // My sense
+        for (String a : array ){
+            System.out.println(a);
+            // System.out.print((a) + " ");
+            
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        ArrayList<String> arraylist = new ArrayList<String>();
+        
+        
+        arraylist.add("Metallica");
+        arraylist.add("Guns' n Roses");
+        arraylist.add("Black Sabbath");
+        arraylist.add("Iron Maiden");
+        arraylist.add("Megadeth");
+
+       // System.out.println(arraylist.get(0));
+         //System.out.println(arraylist.get(3));
+        // System.out.println(arraylist.get(5));
+         
+        // System.out.println(arraylist.size());
+        
+        //arraylist.remove(1);
+        //arraylist.remove("Metallica");
+        
+        /*System.out.println(arraylist.indexOf("Metallica"));
+        System.out.println(arraylist.lastIndexOf("Metallica"));
+        System.out.println(arraylist.indexOf("Dream Theater")); // sonuç -1
+        */
+        
+        //arraylist.set(4,"Megadeth");
+        
+        // yazdir(arraylist);
+       
+        yaz(arraylist); // My sense
+    }
+}
