ec96c63e3e78eee1b467bf300e421a1d62e123c6
Gökhan Karaömeroğlu <80010235+GokhanKaraomeroglu@users.noreply.github.com>
2022-07-24 21:49:29 +0300

    Interface

---
+++ 90-InterfacelerDetay/src/IDeneme.java
@@ -0,0 +1,22 @@
+
+import apple.laf.JRSUIConstants;
+
+        
+
+public interface IDeneme {
+    
+   // private int a= 4;
+    public int a= 4;
+    // public int b;
+    public int b=5;
+    
+    public static String deneme = "Deneme"; // statik burada gereksiz...
+    
+    public final int c = 6;
+    
+    public static void deneme(){
+        
+        System.out.println("Deneme");
+        
+    }
+}
